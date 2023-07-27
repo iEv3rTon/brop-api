78207faef4e163f082309e46d456824b84ffa9c3
iEv3rTon <46271347+iEv3rTon@users.noreply.github.com>
2023-07-27 19:25:35 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,14 @@ app.use(router);
 app.get('/porra', (_req: Request, res: Response) => {
 	return res.send('Express Typescript on Vercel')
 })
+app.get('/', (_req: Request, res: Response) => {
+	return res.send('Express Typescript on Vercel')
+})
+
+app.get('/ping', (_req: Request, res: Response) => {
+	return res.send('pong 🏓')
+})
+
 
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`))
