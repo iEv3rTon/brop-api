3308e9980c65299753a90423cebdf30f50bdcfb3
iEv3rTon <46271347+iEv3rTon@users.noreply.github.com>
2023-07-27 19:18:07 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,9 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+app.get('/porra', (_req: Request, res: Response) => {
+	return res.send('Express Typescript on Vercel')
+})
 
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`))
