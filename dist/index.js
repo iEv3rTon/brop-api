bc3c05c675766ad27a54c064a1472492455421b1
iEv3rTon <46271347+iEv3rTon@users.noreply.github.com>
2023-07-27 19:31:26 -0300

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -6,10 +6,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
+
+import routes from "./src/routes"
+    
 app.get('/', (_req, res) => {
     return res.send('Express Typescript on Vercel');
 });
 app.get('/ping', (_req, res) => {
+    return res.send('pong 🏓');
+});
+app.get('/teste', (_req, res) => {
     return res.send('pong 🏓');
 });
 app.listen(port, () => {
