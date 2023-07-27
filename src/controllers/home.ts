3d44dd95f5d216380bd001aabca6838438f2fd75
iEv3rTon <46271347+iEv3rTon@users.noreply.github.com>
2023-07-27 18:42:56 -0300

    Update home.ts

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express'
 
-export class FeedController {
+export class Home {
     async handle(request: Request, response: Response) {
-        const { searchString, skip, }
+        return response.status(200).json({
+    title: "Express Testing",
+    message: "The app is working properly!",
+  });
     }
 }
