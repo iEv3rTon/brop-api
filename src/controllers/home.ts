6369caf4d1ac06ccfcb9de3308c5542a1d732293
iEv3rTon <46271347+iEv3rTon@users.noreply.github.com>
2023-07-27 18:40:56 -0300

    Update home.ts

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,1 +1,7 @@
+import { Request, Response } from 'express'
 
+export class FeedController {
+    async handle(request: Request, response: Response) {
+        const { searchString, skip, }
+    }
+}
